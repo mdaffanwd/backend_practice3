7ca1f168a267311fb3e1dfaa5453df9116363e09
mdaffanwd <mohammedaffan2305@gmail.com>
2025-02-02 10:11:54 +0530

    modify healthcheck.routes.js

--- src/routes/healthcheck.routes.js
+++ src/routes/healthcheck.routes.js
@@ -4,7 +4,7 @@ import healthcheck from '../controllers/healthcheck.controller.js';
 const router = Router();
 
 router.route('/').get(healthcheck);
-router.route('/test').get(healthcheck);
+// router.route('/test').get(healthcheck);
 
 
 export default router
