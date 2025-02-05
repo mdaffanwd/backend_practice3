82839a6e5a3afae2bc9b955f358ee539d4ddeb9d
mdaffanwd <mohammedaffan2305@gmail.com>
2025-02-05 20:55:46 +0530

    add getCurrentUser cntrlr in user.cntrlr.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -268,7 +268,9 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"))
 })
 
-const getCurrentUser = asyncHandler(async (req, res) => { })
+const getCurrentUser = asyncHandler(async (req, res) => { 
+  return res.status(200).json(new ApiResponse(200, req.user, "Current user details"))
+})
 
 const updateAccountDetails = asyncHandler(async (req, res) => { })
 
