aae1e9a7fd5871f3f1e169795c4cedb4c6d46a08
mdaffanwd <mohammedaffan2305@gmail.com>
2025-02-13 11:29:08 +0530

    change/add comments

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,4 +4,20 @@ const asyncHandler = (reqHandler) => {
   };
 };
 
-export {asyncHandler} ;
+export { asyncHandler };
+
+// for understanding.
+// above code's working but with async, try/catch
+// function asyncHandler(func) {
+//     return async function (req, res, next  ) {
+//         try {
+//             await func(req, res,next)
+//         } catch (error) {
+              // console.log(error.message)
+              // res.status(error.code || 500).json({
+              //   success: false,
+              //   message: error.message
+              // })
+//         }
+//     }
+// }
